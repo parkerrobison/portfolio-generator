43abe5eb4ed994f1f70979085c9f062d30246ed0
probison <parker.robison@hotmail.com>
2020-06-22 23:15:24 -0600

    modularized the js. JS now can generate HTML file from template

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js'); 
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+//these two variables are arrays.
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
+//this is the condensed version of the code above. It is called assignment destructuring.
+//it assigns elements of an array in a single expression
+const [name, github] = profileDataArgs;
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('================');
 
-printProfileData(profileDataArgs)
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs)
+
+
+//hardcoded
+// the () is where a parameter would be
+//const generatePage = () => 'Name: Jane, Github: janehub';
+
+// interpolated code
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+
+// multi line code. Put 'return' wherever you would like a line break to occur.
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+})
+
