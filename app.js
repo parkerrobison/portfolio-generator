d10441dd322efab6b26e61e0d8c6cee89735bd3e
probison <parker.robison@hotmail.com>
2020-06-23 23:59:56 -0600

    made multiple entires possible in the form

--- app.js
+++ app.js
@@ -1,14 +1,89 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: "input",
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+        }
+    ]);
+};
+
+const promptProject = portfolioData => {
+    // this checks if an array has been created.
+    if(!portfolioData.projects){
+    portfolioData.projects = [];
+    }
+    console.log(`
+=================
+Add a New Project
+=================
+`);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you do this project with? (Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter GitHub link to your project. (Required)',
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
     ])
-    .then(answers => console.log(answers));
+    //this pushes data up to the project array
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        // if confirmAddProject is true it runs the function again.
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    })
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
+    
+
 // const fs = require('fs');
 // // this statement the object in module.exports assignment will be reassigned to this variable.
 // const generatePage = require('./src/page-template.js'); 
